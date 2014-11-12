9081ccbfdcdb6485439b0c913b9037df00eade6c
Daniel Montoya <dsmontoyam@gmail.com>
2014-11-12 11:00:49 -0600

    Marcador draggable para seleccionar última localización del perro.

--- public/javascripts/getLocation.js
+++ public/javascripts/getLocation.js
@@ -2,11 +2,13 @@
 var map = document.querySelector("#map");
 navigator.geolocation.getCurrentPosition(GetLocation);
 function GetLocation(location) {
-    var geo = function(latitude, longitude, accuracy){
-    this.latitude = location.coords.latitude
-    this.longitude = location.coords.longitude
-    this.accuracy = location.coords.accuracy
-}
-    map.location = new geo();
-    console.log(map.location);
+	    var geo = function(latitude, longitude, accuracy){
+		    this.latitude = location.coords.latitude
+		    this.longitude = location.coords.longitude
+		    this.accuracy = location.coords.accuracy
+		}
+	map.location = new geo();
+	newPinMarker = $('#new_pin_marker');
+	newPinMarker.attr('latitude', map.location.latitude);
+	newPinMarker.attr('longitude', map.location.longitude);
 }
